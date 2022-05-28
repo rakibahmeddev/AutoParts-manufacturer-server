657eac4f750732fb4ef60f27f7335007aa8891f1
rakib ahmed <rakibahmed.dev@gmail.com>
2022-05-28 12:08:51 +0600

    nodemon server is ready to start

---
+++ index.js
@@ -0,0 +1,11 @@
+//basic formations
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("AutoParts server running successfully");
+});
+app.listen(port, () => {
+  console.log(`server is running ${port}`);
+});
